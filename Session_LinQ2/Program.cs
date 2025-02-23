e1b189cd51f49fa9b7696c6a3b4826d359b5f78f
Adel Kassem <adel.erorr159@gmail.com>
2025-02-23 23:36:27 +0200

    UpdateNewTask

--- Session_LinQ2/Program.cs
+++ Session_LinQ2/Program.cs
@@ -99,8 +99,21 @@ class Program
         // foreach(var p in Resutl) Console.WriteLine(p);
         #endregion
         
-        #region Q11 
-        //  لسة البشمهندس مشرحهاش 
+        #region Q11
+
+        // var Result = from p in ProductList
+        //     group p by p.Category
+        //     into g
+        //     let cheapest = g.Min(p => p.UnitPrice)
+        //     from p2 in ProductList
+        //     where p2.UnitPrice == cheapest
+        //     select p2;
+        // foreach (var product in Result)
+        // {
+        //     Console.WriteLine(product);
+        //     
+        // }
+
         #endregion
 
         #region Q12
@@ -110,8 +123,9 @@ class Program
         // foreach(var p in Resutl) Console.WriteLine(p);
 
         #endregion
-        
+
         #region Q13
+
         // var Resutl = ProductList.GroupBy(p =>p.Category).Select(p2=> new
         //     { p2.Key, MostExpensivePrice=p2.Where(p3=>p3.UnitPrice==p2.Max(p4 => p4.UnitPrice)) });
         // foreach (var p in Resutl)
@@ -119,24 +133,28 @@ class Program
         //     Console.WriteLine($"CategoryName: {p.Key}");
         //     foreach (var product in p.MostExpensivePrice) Console.WriteLine($"ProductName: {product.ProductName} UnitPrice: {product.UnitPrice}");
         // }
+
         #endregion
-        
+
         #region Q14
+
         // var Resutl = ProductList.Select(p => new
         //     { p.Category, AveragePrice=ProductList.Where(p2 => p2.Category == p.Category).Average(price => price.UnitPrice) }).Distinct();
         // foreach(var p in Resutl) Console.WriteLine(p);
+
         #endregion
 
         #endregion
-        
+
         #region SetOperators
+
         #region Q1
 
         // var Result = ProductList.Select(p=>p.Category).Distinct();
         // foreach(var category in Result) Console.WriteLine(category);
 
         #endregion
-        
+
         #region Q2
 
         // var Result = ProductList.Select(p => p.ProductName[0]).Union(CustomerList.Select(c => c.CustomerName[0]))
@@ -144,71 +162,43 @@ class Program
         // foreach(var letter in Result) Console.WriteLine(letter);
 
         #endregion
-        
+
         #region Q3
 
         // var Result = ProductList.Select(p => p.ProductName[0]).Intersect(CustomerList.Select(c => c.CustomerName[0]));
         // foreach(var letter in Result) Console.WriteLine(letter);
+
         #endregion
-        
+
         #region Q4
 
         // var Result = ProductList.Select(p => p.ProductName[0]).Except(CustomerList.Select(c => c.CustomerName[0]));
         // foreach(var letter in Result) Console.WriteLine(letter);
+
         #endregion
-        
+
         #region Q5
+
         // var Result = ProductList.Select(p => p.ProductName.Substring(p.ProductName.Length-3))
         //     .Concat(CustomerList.Select(c => c.CustomerName.Substring(c.CustomerName.Length-3)));
         // foreach(var letter in Result) Console.Write($"{letter} ");
-        #endregion
-
-        #endregion
-        
-        #region PartitioningOperators
-        #region Q1
-
-        // var Result = CustomerList.Where(c => c.City == "Washington").SelectMany(o => o.Orders).Take(3);
-        // foreach(var c in Result) Console.WriteLine(c);
-        #endregion
-        
-        #region Q2
-        // var Result = CustomerList.Where(c => c.City == "Washington").SelectMany(o => o.Orders).Take(2);
-        // foreach(var c in Result) Console.WriteLine(c);
-        #endregion
-        
-        #region Q3
-        // int [] numbers = {5, 4, 1, 3, 9, 8, 6, 7, 2, 0};
-        // var Result = numbers.TakeWhile((num, i) => num > i);
-        // foreach(var num in Result) Console.WriteLine(num);
-
-        #endregion
-        
-        #region Q4
-        // int [] numbers = {5, 4, 1, 3, 9, 8, 6, 7, 2, 0};
-        // var Result = numbers.SkipWhile(num => num % 3 != 0);
-        // foreach(var num in Result) Console.WriteLine(num);
-
-        #endregion
-        
-        #region Q5
-        // int [] numbers = {5, 4, 1, 3, 9, 8, 6, 7, 2, 0};
-        // var Result = numbers.SkipWhile((num, i) => num > i);
-        // foreach(var number in Result) Console.WriteLine(number);
 
         #endregion
 
         #endregion
-        
+
         #region QuantifiersOperators
+
         #region Q1
+
         // string[] Arr = File.ReadAllLines("dictionary_english.txt");
         // var Result = Arr.Any(a => a == "ei");
         // Console.WriteLine(Result);
 
         #endregion
-        
+
         #region Q2
+
         // var Resutl = ProductList.GroupBy(p => p.Category).Where(p2 => p2.Any(p => p.UnitsInStock == 0))
         //     .Select(p3=>new{p3.Key,Products=p3} );
         // foreach (var p in Resutl)
@@ -216,9 +206,11 @@ class Program
         //     Console.WriteLine(p.Key);
         //     foreach(var product in p.Products) Console.WriteLine(product);
         // }
+
         #endregion
-        
+
         #region Q3
+
         // var Resutl = ProductList.GroupBy(p => p.Category).Where(p2 => p2.All(p => p.UnitsInStock > 0))
         //     .Select(p3=>new{p3.Key,Products=p3} );
         // foreach (var p in Resutl)
@@ -226,9 +218,11 @@ class Program
         //     Console.WriteLine(p.Key);
         //     foreach(var product in p.Products) Console.WriteLine(product);
         // }
+
         #endregion
+
         #endregion
-        
+
     }
 }
 
