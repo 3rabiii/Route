008f3047509f14cad450ee5fdd577741b2974689
Adel Mohamed <adel.erorr159@gmail.com>
2025-03-23 02:15:11 +0200

    Task

--- TaskSessionMVC2/Controllers/HomeController.cs
+++ TaskSessionMVC2/Controllers/HomeController.cs
@@ -20,12 +20,5 @@ public class HomeController: Controller
     {
         return View();
     }
-    public IActionResult Login()
-    {
-        return View();
-    }
-    public IActionResult Register()
-    {
-        return View();
-    }
+
 }
---
+++ TaskSessionMVC2/Controllers/TestController.cs
@@ -0,0 +1,41 @@
+using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+ 
+namespace Session2.Controllers;
+ 
+public class TestController: Controller
+{
+    // public string GetMovie(int id)
+    // {
+    //     return "adel mohamed";
+    // }
+    //
+    // public string Index()
+    // {
+    //     return "Hello, From Index";
+    // }
+    // public ContentResult Index()
+    // {
+    //   ContentResult result = new ContentResult();
+    //   result.Content = "adel mohamed";
+    //   result.ContentType = "text/html";
+    //   return result;
+    // }
+    // public RedirectResult Redirect()
+    // {
+    //     RedirectResult result = new RedirectResult("http://localhost:5126/Movies");
+    //     return result;
+    // }
+ 
+    // public IActionResult Index()
+    // {
+    //     return Content("adel kassem....5");
+    // }
+     
+    // public IActionResult Redirect()
+    // {
+    // return Redirect("http://localhost:5126/Movies");
+    //return RedirectToAction("Index");
+    //return RedirectToRoute(new{Controller = "Movies", Action = "Index"});
+    // }
+}
